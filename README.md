f2a8cc8804982b3d0e751d686e7dc09532b4e8c8
RishadYamnoor <rishad.yamnoor@gmail.com>
2017-12-06 23:37:23 +0530

    READ ME included

---
+++ README.md
@@ -0,0 +1,15 @@
+
+## Feed Reader
+---
+#### Project Goal
+Jasmine testing framework was used write series of test suites against pre-existing application.
+This is part of Udacity's Frontend Web Developement nanodegree curriculum projects.
+
+#### Skills needed:
+- Jasmine test framework.
+- Understanding of JavaScript Callback functions and Event Handling.
+- DOM manipulation.
+- jQuery
+
+#### How to view?
+You can view this project either by downloading the repo and opening index.html or simply [click this.](https://evolofthings.github.io/Feed-Reader/)
--- js/app.js
+++ js/app.js
@@ -32,14 +32,6 @@ function init() {
     loadFeed(0);
 }
 
-/* This function performs everything necessary to load a
- * feed using the Google Feed Reader API. It will then
- * perform all of the DOM operations required to display
- * feed entries on the page. Feeds are referenced by their
- * index position within the allFeeds array.
- * This function all supports a callback as the second parameter
- * which will be called after everything has run successfully.
- */
  function loadFeed(id, cb) {
      var feedUrl = allFeeds[id].url,
          feedName = allFeeds[id].name;
@@ -60,11 +52,6 @@ function init() {
                  title.html(feedName);   // Set the header text
                  container.empty();      // Empty out all previous entries
 
-                 /* Loop through the entries we just loaded via the Google
-                  * Feed Reader API. We'll then parse that entry against the
-                  * entryTemplate (created above using Handlebars) and append
-                  * the resulting HTML to the list of entries on the page.
-                  */
                  entries.forEach(function(entry) {
                      container.append(entryTemplate(entry));
                  });
@@ -88,10 +75,6 @@ function init() {
  */
 google.setOnLoadCallback(init);
 
-/* All of this functionality is heavily reliant upon the DOM, so we
- * place our code in the $() function to ensure it doesn't execute
- * until the DOM is ready.
- */
 $(function() {
     var container = $('.feed'),
         feedList = $('.feed-list'),
@@ -99,12 +82,6 @@ $(function() {
         feedId = 0,
         menuIcon = $('.menu-icon-link');
 
-    /* Loop through all of our feeds, assigning an id property to
-     * each of the feeds based upon its index within the array.
-     * Then parse that feed against the feedItemTemplate (created
-     * above using Handlebars) and append it to the list of all
-     * available feeds within the menu.
-     */
     allFeeds.forEach(function(feed) {
         feed.id = feedId;
         feedList.append(feedItemTemplate(feed));
@@ -112,10 +89,6 @@ $(function() {
         feedId++;
     });
 
-    /* When a link in our feedList is clicked on, we want to hide
-     * the menu, load the feed, and prevent the default action
-     * (following the link) from occurring.
-     */
     feedList.on('click', 'a', function() {
         var item = $(this);
 
@@ -124,9 +97,6 @@ $(function() {
         return false;
     });
 
-    /* When the menu icon is clicked on, we need to toggle a class
-     * on the body to perform the hiding/showing of our menu.
-     */
     menuIcon.on('click', function() {
         $('body').toggleClass('menu-hidden');
     });
